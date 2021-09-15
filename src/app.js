3567f542edbb7206347b0d74cb5037130f714164
Ali Shah <s.ashah@outlook.com>
2021-09-15 03:51:37 -0400

    [Update] finished server code

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 import express from 'express';
 import fetch from 'node-fetch';
 
 dotenv.config()
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 
-app.post('/', async (req, res) => {
-  let data = await fetch(req.body.url + `&appid=${process.env.API_KEY}`);
-  data = await data.json();
+app.use(express.static(join(__dirname, '..', 'build')));
+
+app.get('/weather', async (req, res) => {
+  const { code, units } = req.query;
+
+  const urlBase = 'https://api.openweathermap.org/data/2.5/weather';
+  const urlQuery = `?appid=${process.env.API_KEY}&zip=${code}&units=${units}`;
+
+  let data = await fetch(urlBase + urlQuery);
+  data = data.json();
+
   return res.json(data);
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(3000);
